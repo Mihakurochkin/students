9d5bd8d153e2ca442722d432fc2c0c4c6d18c6a2
mihakurochkin <kurochkinmy@gmail.com>
2020-05-26 16:03:08 +0300

    Create architecture

--- index.js
+++ index.js
@@ -1,38 +1,19 @@
-let students = [
-  {
-    firstName: 'Jevgeniy',
-    lastName: 'Kurochkin',
-    grade1: 11,
-    grade2: 9,
-  },
-  {
-    firstName: 'Mykhailo',
-    lastName: 'Kurochkin',
-    grade1: 8,
-    grade2: 6,
-  },
-  {
-    firstName: 'Halyna',
-    lastName: 'Frontai',
-    grade1: 11,
-    grade2: 12,
-  },
-  {
-    firstName: 'James',
-    lastName: 'Dean',
-    grade1: 5,
-    grade2: 9,
-  }
-];
-students = students.map((value) => {
-  value.total = Math.round((value.grade1 + value.grade2) / 2);
-  return value;
-});
+// state
+let students = [];
+function setStudents(arr) {
+  students = arr;
+  update();
+}
 
-const average = students.reduce((acc, student) => acc + student.total, 0)/ students.length;
-$(() => {
+// update
+function update() {
+  const average = students.reduce((acc, student) => acc + student.total, 0)/ students.length;
   $('#summary').text(`Average: ${average}`);
-  students.forEach((student) => {
+
+  students.map((value) => {
+    value.total = Math.round((value.grade1 + value.grade2) / 2);
+    return value;
+  }).forEach((student) => {
     $('#table').append($(`
       <div class="row">
           <div class="cell">${student.lastName}</div>
@@ -42,5 +23,39 @@ $(() => {
           <div class="cell">${student.total}</div>
       </div>
     `));
-  });
+  })
+}
+
+// main code
+$(() => {
+  let array = [
+    {
+      firstName: 'Jevgeniy',
+      lastName: 'Kurochkin',
+      grade1: 11,
+      grade2: 9,
+    },
+    {
+      firstName: 'Mykhailo',
+      lastName: 'Kurochkin',
+      grade1: 8,
+      grade2: 6,
+    },
+    {
+      firstName: 'Halyna',
+      lastName: 'Frontai',
+      grade1: 11,
+      grade2: 12,
+    },
+    {
+      firstName: 'James',
+      lastName: 'Dean',
+      grade1: 5,
+      grade2: 9,
+    }
+  ];
+
+  setTimeout(() => {
+    setStudents(array);
+  }, 3000)
 });
